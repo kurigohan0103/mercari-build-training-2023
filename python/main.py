7b2a2e36b92b9910fee904d5d217e453dffc67ca
kurigohan0103 <ayako.kurita.2001@gmail.com>
2023-05-12 12:34:49 +0900

    step3

--- python/main.py
+++ python/main.py
@@ -1,13 +1,19 @@
 import os
 import logging
 import pathlib
-from fastapi import FastAPI, Form, HTTPException
+from fastapi import FastAPI, Form, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+import hashlib
+import shutil
+import json
+
+filename = "items.json"
+
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
 origins = [ os.environ.get('FRONT_URL', 'http://localhost:3000') ]
 app.add_middleware(
@@ -23,13 +29,46 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...), category: str = Form(...), file: UploadFile = File(...)):
+    with open(file.filename, 'rb') as f:
+        sha256 = hashlib.sha256(f.read()).hexdigest()
+
+    fname = file.filename
+    upload_dir = open(os.path.join("images", fname),'wb+')
+    shutil.copy(fname, 'images/' + sha256 + '.jpg')
+    upload_dir.close()
+    
+    item_tsuika = {'name': name, 'category': category, 'image_filename': filename + '.jpg'}
+    with open(filename, 'r') as f:
+        read_data = json.load(f)
+
+    
+    read_data["items"].append(item_tsuika)
+   
+    
+    with open(filename, 'w') as f:
+        json.dump(read_data, f)
+    
+    
     logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+    return {"message": f"item received: {name}, {category}, {sha256 + '.jpg'}"}
+
+@app.get("/items")
+def root():
+    with open(filename, 'r') as f:
+        read_data = json.load(f)
+    return read_data
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    with open(filename, 'r') as f:
+        read_data = json.load(f)
+    return read_data["items"][item_id-1]
+    
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
-    # Create image path
+   
     image = images / image_filename
 
     if not image_filename.endswith(".jpg"):
